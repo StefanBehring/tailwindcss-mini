d99bce6985439d1ed19cf25d1e4e9fb5426d3eec
StefanBehring <chadhz@gmx.de>
2021-12-13 16:25:57 +0100

    Further improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [],
+	theme: {
+		debugScreens: {
+			position: ['top', 'left'],
+		},
+		extend: {
+			fontFamily: {
+				ueberschrift: ['Oswald'],
+			},
+			colors: {
+				hauptfarbe: '#212f49',
+			},
+		},
+	},
+	plugins: [require('tailwindcss-debug-screens')],
 }
